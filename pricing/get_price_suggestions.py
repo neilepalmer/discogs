78a9022ec61360da430894e39ac6de0ed88bc9bd
Neil Palmer <npalmer@acm.org>
2019-10-12 18:01:17 -0400

    fixed the parsing logic and parameterized

--- pricing/get_price_suggestions.py
+++ pricing/get_price_suggestions.py
@@ -1,26 +1,87 @@
+import argparse
 import csv
-import jq
 import requests
+from time import sleep
 
-url = 'https://www.googleapis.com/qpxExpress/v1/trips/search?key=mykeyhere'
-with open('neilepalmer-collection-20191011-1851.csv') as csv_file:
+base_url = 'https://api.discogs.com//marketplace/price_suggestions/'
+
+headers = {'content-type': 'application/json', 'Accept-Charset': 'UTF-8', 'user-agent': "FooBarApp/3.0"}
+
+field_names = {"Catalog#", "Artist", "Title", "Label", "Format", "Rating", "Released", "release_id",
+                "CollectionFolder", "Date Added", "Collection Media Condition", "Collection Sleeve Condition",
+                "Collection Notes", "Collection Location", "Price"}
+
+parser = argparse.ArgumentParser(description='Expecting collection filename and Discogs token')
+parser.add_argument("--file", required=True, type=str, help="Filename of the inventory export from Discogs to parse")
+parser.add_argument("--token", required=True, type=str, help="Your personal access token to Discogs")
+
+args = parser.parse_args()
+filename = args.file
+token = args.token
+headers.update({'Authorization': 'Discogs token=' + token})
+
+with open(filename) as csv_file, open('results.csv', 'w', newline='') as results_file:
+
+    # Read collection file
     csv_reader = csv.DictReader(csv_file)
+    csv_writer = csv.DictWriter(results_file, field_names)
+
     line_count = 0
     for row in csv_reader:
 
+        # Reset current price
+        price = 0
+
+        # Skips headers
         if line_count == 0:
-            print(f'Column names are {", ".join(row)}')
+            # print(f'Column names are {", ".join(row)}')
             line_count += 1
 
-        if (row["CollectionFolder"] == "Flood Damage"):
-            if (row["Collection Media Condition"] == "") or (row["Collection Sleeve Condition"] == ""):
-                print(f'\t{row["Artist"]} {row["Title"]} Media : {row["Collection Media Condition"]}. Sleeve : {row["Collection Sleeve Condition"]} ')
+        # Filter only on Flood Damage for this current use case
+        # TODO: Run this to check arbitrage opportunities?
+        if row["CollectionFolder"] != "Flood Damage":
+            print("Not Flood Damage - skipping!")
+            continue;
 
-            # print(f'\t{row["Artist"]} {row["Title"]} Media : {row["Collection Media Condition"]}. Sleeve : {row["Collection Sleeve Condition"]} ')
+        # Check for set media and sleeve condition
+        if (row["Collection Media Condition"] == "") or (row["Collection Sleeve Condition"] == ""):
+            print(f'Invalid Catalog Item: {row["Artist"]} {row["Title"]} Media : {row["Collection Media Condition"]}. '
+                  f'Sleeve : {row["Collection Sleeve Condition"]} ')
+            continue
 
-        # payload = open("request.json")
-        # headers = {'content-type': 'application/json', 'Accept-Charset': 'UTF-8'}
-        # r = requests.post(url, data=payload, headers=headers)
+        # Retrieve suggested pricing
+        url = base_url + row["release_id"]
+        r = requests.get(url, headers=headers)
 
+        # Check rate limiting
+        rate_limit = r.headers['X-Discogs-Ratelimit']
+        rate_limit_used = r.headers['X-Discogs-Ratelimit-Used']
+        rate_remaining = r.headers['X-Discogs-Ratelimit-Remaining']
+        print(f'Limit: {rate_limit} Used: {rate_limit_used} Remaining: {rate_remaining}')
+
+        if rate_remaining is not None and int(rate_remaining) < 10:
+            print('Sleep sleep sleeping')
+            sleep(10)
+
+        # Add some parsing based on condition (media? sleeve? both?)
+        # print(r.json())
+
+        # print(row["Collection Media Condition"])
+        result = r.json().get(row["Collection Media Condition"])
+        if result is None:
+            print(f'\t{row["Artist"]} {row["Title"]} Media : {row["Collection Media Condition"]}. Price : UNKNOWN')
+            continue
+        elif result.get('currency') == 'USD':
+            price = result.get('value')
+        else:
+            print(f"Non USD Currency! {result}")
+
+        row["Price"] = round(price, 2)
+        print(row)
+        csv_writer.writerow(row)
+
+        # print(f'\t{row["Artist"]} {row["Title"]} Media : {row["Collection Media Condition"]}. Price : {round(price, 2)}')
+
+        # Rewrite output
         line_count += 1
     print(f'Processed {line_count} lines.')
